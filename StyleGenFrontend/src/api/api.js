66cadd498e84b889c7c84bea16b62f3432bd0ffb
Raju204116 <raju204116@gmail.com>
2026-05-16 00:18:43 +0600

    Fix duplicate api path

--- StyleGenFrontend/src/api/api.js
+++ StyleGenFrontend/src/api/api.js
@@ -7,15 +7,17 @@ import axios from 'axios';
 export const API_URL = "https://rj-stylegen.onrender.com/api";
 
 const api = axios.create({
-  baseURL: `${API_URL}/api`,
+  baseURL: API_URL,
 });
 
-// Adding an Interceptor 
+// Adding an Interceptor
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 }, (error) => {
   return Promise.reject(error);
